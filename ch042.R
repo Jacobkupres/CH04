459c2caad818e074e51d17da12e39ff2eb385b12
jacobkupres <jkupres46@siu.edu>
2017-07-09 23:25:10 -0500

    adding ch042 to the project

---
+++ ch042.R
@@ -0,0 +1,56 @@
+#chapter 4.2 Variable
+
+#Two assignment operators
+x<-2
+x
+y=5
+y
+
+#left-hand side variable vs. right-hand side variable
+z<-3
+z
+3<-Z
+Z
+
+#Successize assigment for assigning a value to multiple varaible simultaneously
+a<-b<-7
+a
+b
+
+#The assign function
+assign("j",4)
+j
+
+#Variable naming convention
+#Any combination of alphanumeric characters along with periods and undersoures.
+x123<-100
+x123
+
+x_value <-200
+x_value
+
+X-value<-300
+x_value
+
+x_value<-400
+x_value
+
+x_value<-500
+x_value
+
+#variable name cannot start with a number or an undersource
+#1x<-400
+#_value<-100
+
+#Removing a Variable
+j<-100
+j
+rm(j)
+j<-200
+gc(j)
+
+#R variable names are case sensitive
+x1<-17
+x1
+X1
+
